722e34f5dc65cee1da5edee282b42a9243869083
Konpa <pavelkonoplyanikov@yandex.by>
2021-04-02 13:44:33 +0300

    assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,6 @@
+import  { strict as assert } from 'assert';
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.strictEqual(capitalize(''), '');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
-
-console.log('Все тесты пройдены!');
+assert.strictEqual(capitalize('hello'), 'Hello');
